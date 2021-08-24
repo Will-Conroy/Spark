9a85d130b12f967bd83b6ce9b5873b941b099654
Will-Conroy <35853403+Will-Conroy@users.noreply.github.com>
2021-08-24 17:40:15 +0100

    Populates stocks form commandline

--- code/magicVWAP.cpp
+++ code/magicVWAP.cpp
@@ -1,3 +1,39 @@
 
+#include "magicVWAP.h"
+#include "datapath.h"
+#include "input.h"
 
 
+
+void populatedStocksFromCommandLine(Stocks& stocks){
+    
+    bool populated = false;
+
+    do{
+        std::cout << "What file do you want to read in:" << std::endl;
+       
+        for(int i = 0; i < MAGIC_VWAP::InputFiles::NUM_DATASETS; i++){
+            std::cout << '{' << i << " | " << MAGIC_VWAP::InputFiles::DATASETS[i].NAME << " | " << MAGIC_VWAP::InputFiles::DATASETS[i].FILE << "}, ";
+        }
+        std::cout << std::endl;
+        std::string fileNum;
+        std::cin >> fileNum;
+
+        try
+        {
+            int intFileNum = stoi(fileNum);
+            InputFile input(MAGIC_VWAP::InputFiles::DATASET_FILE_DIR + MAGIC_VWAP::InputFiles::DATASETS[intFileNum].FILE);
+            stocks.popluateFormCSV(input.open(), MAGIC_VWAP::InputFiles::DATASETS[intFileNum].COLS);
+            populated = true;
+        }
+        catch(std::runtime_error& e)
+        {
+            std::cout << e.what() << std::endl;
+        }catch(...){
+            std::cout << "Invlaid responce, please enter an int that corraspons to a file" << std::endl;
+        }
+        
+    }while(!populated);
+
+    
+}
